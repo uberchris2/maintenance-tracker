dea7cd8e8f50599f499581f2cf94914de555cef8
Chris Carpenter <chris@uberchris.com>
2019-04-09 13:37:22 -0500

    correct prod uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiEndpoint: 'https://maintenance-tracker-api.azurewebsites.net/',
-  origin: 'https://maintenancetrackersa.z19.web.core.windows.net'
+  origin: 'https://maintenancetrackersa.z19.web.core.windows.net/'
 };
