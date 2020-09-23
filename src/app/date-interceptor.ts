c5f66f41c66924b5f5fc35bbbc3914226940a16f
Chris Carpenter <chris@uberchris.com>
2020-09-23 16:14:46 -0700

    clean up some build warnings

--- src/app/date-interceptor.ts
+++ src/app/date-interceptor.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class DateInterceptor implements HttpInterceptor {
   iso8601 = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/;
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).do((event: HttpEvent<any>) => {
-      if (event instanceof HttpResponse) {
-        const body = event.body;
-        this.convertToDate(body);
-      }
-    }, (err: any) => {
-      if (err instanceof HttpErrorResponse) {
-        if (err.status === 401) {
-        }
-      }
-    });
-  }
+    return next.handle(req).pipe(
+        tap((event: HttpEvent<any>) => {
+            if (event instanceof HttpResponse) {
+                const body = event.body;
+                this.convertToDate(body);
+            }
+        }, (err: any) => {
+            if (err instanceof HttpErrorResponse) {
+                if (err.status === 401) {
+                }
+            }
+        }),
+    );
+}
 
   convertToDate(body) {
     if (body === null || body === undefined) {
