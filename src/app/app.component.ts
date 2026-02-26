09ae4b0d884d20ac7d7317f4afe447c58395924e
Chris Carpenter <chris@uberchris.com>
2026-02-26 13:07:37 -0800

    chore: template cleanup (fix duplicate form, broken HTML, invalid table, routerLinkActive)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { Router, NavigationEnd, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
 import { InteractionStatus } from '@azure/msal-browser';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
+
 import { NgxSpinnerComponent } from 'ngx-spinner';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,13 +15,12 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [NgxSpinnerComponent, RouterLink, NgbCollapse, FaIconComponent, RouterOutlet]
+    imports: [NgxSpinnerComponent, RouterLink, RouterLinkActive, NgbCollapse, FaIconComponent, RouterOutlet]
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'maintenance-ui';
   loggedIn = false;
   // isIframe: boolean;
-  currentUrl: string = '';
   navbarCollapsed = true;
   faSignOutAlt = faSignOutAlt;
   private readonly destroying$ = new Subject<void>();
@@ -44,11 +44,11 @@ export class AppComponent implements OnInit, OnDestroy {
     //   takeUntil(this.destroying$)
     // ).subscribe((result: EventMessage) => this.router.navigate(['unauthorized', result?.error?.message]));
 
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.currentUrl = event.url;
-        this.navbarCollapsed = true;
-      }
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      takeUntil(this.destroying$)
+    ).subscribe(() => {
+      this.navbarCollapsed = true;
     });
   }
 
