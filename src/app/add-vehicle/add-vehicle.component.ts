cbc37008e8b6a736804cdf6bffbbacc5382553c2
Chris Carpenter <chris@uberchris.com>
2026-02-26 14:28:16 -0800

    update add vehicle component to not use car query

--- src/app/add-vehicle/add-vehicle.component.ts
+++ src/app/add-vehicle/add-vehicle.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { YearMakeModelService, Make } from '../services/year-make-model.service';
+import { Component, inject } from '@angular/core';
 import { VehicleService } from '../services/vehicle.service';
 import { Vehicle } from '../models/vehicle';
 import { Router, RouterLink } from '@angular/router';
@@ -12,59 +11,26 @@ import { FormsModule } from '@angular/forms';
     styleUrl: './add-vehicle.component.css',
     imports: [RouterLink, FormsModule]
 })
-export class AddVehicleComponent implements OnInit {
-
-  years: number[] | undefined;
-  makes: Make[] | undefined;
-  models: string[] | undefined;
+export class AddVehicleComponent {
 
   year: number | undefined;
-  make: Make | undefined;
-  model: string | undefined;
+  make = '';
+  model = '';
   name = '';
   mileage: number | undefined;
 
-  private yearMakeModelService = inject(YearMakeModelService);
   private vehicleService = inject(VehicleService);
   private router = inject(Router);
 
-  ngOnInit() {
-    this.yearMakeModelService.getYears().subscribe(years => this.years = years);
-  }
-
-  yearSelected() {
-    this.makes = undefined;
-    this.models = undefined;
-    this.make = undefined;
-    this.model = undefined;
-    this.name = '';
-    if (this.year) {
-      this.yearMakeModelService.getMakes(this.year).subscribe(makes => this.makes = makes);
-    }
-  }
-
-  makeSelected() {
-    this.models = undefined;
-    this.model = undefined;
-    this.name = '';
-    if (this.make) {
-      this.yearMakeModelService.getModels(this.year!, this.make.make_id).subscribe(models => this.models = models);
-    }
-  }
-
-  modelSelected() {
-    if (this.model) {
-      this.name = `${this.year} ${this.make!.make_display} ${this.model}`;
-    } else {
-      this.name = '';
-    }
+  updateName() {
+    this.name = `${this.year || ''} ${this.make} ${this.model}`;
   }
 
   add() {
     const newVehicle: Vehicle = {
       year: this.year!,
-      make: this.make!.make_display,
-      model: this.model!,
+      make: this.make,
+      model: this.model,
       name: this.name,
       mileage: this.mileage!,
       shared: false
